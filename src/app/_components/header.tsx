cf3aaac8ce1be9afe112f4197c8d241faaa1c47d
Maxwell Braun <maxbraun175@gmail.com>
2024-11-14 17:51:12 -0500

    fix(menu): add correct links to dropdown menu

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -10,21 +10,22 @@ export async function Header() {
   return (
     <nav className="bg-blue-800 text-white">
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center gap-5">
+        <Link href="/" className="flex items-center gap-5">
           <Image alt="Tampa Devs" width={40} height={40} src={logo} className="h-10 w-10" />
           <h1 className="text-xl font-medium">Tampa Devs Mentorship</h1>
-        </div>
+        </Link>
 
         <div className="flex gap-4">
-          {!session && (
+          {!session ? (
             <div className="flex items-center gap-4">
               <Link href="api/auth/signin">Sign In</Link>
               <Link href="api/auth/signin" className="rounded bg-blue-600 px-3 py-2">
                 Sign Up
               </Link>
             </div>
+          ) : (
+            <Menu session={session} />
           )}
-          <Menu session={session} />
         </div>
       </div>
     </nav>
