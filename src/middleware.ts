6b1f1c8f7b561a5f8542d92cb34ec5efe569685f
Justin Linn <justin@linnjs.com>
2024-11-18 22:22:52 -0500

    feat(auth): Add login redirect expires at 60 sec

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,17 @@ import { routes } from '~/routes';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('authjs.session-token');
+  const hasRedirected = req.cookies.get('has-redirected');
   const url = req.nextUrl.clone();
 
   // Check if the user is logged in
   if (token) {
-    // Check if the user is accessing the root URL
-    if (url.pathname === '/') {
-      // Redirect to dashboard if accessing the root URL for the first time
-      return NextResponse.redirect(new URL(routes.dashboard.home(), req.url));
+    // Check if the user is accessing the root URL and hasn't been redirected yet
+    if (url.pathname === '/' && !hasRedirected) {
+      // Set a cookie to indicate the user has been redirected
+      const response = NextResponse.redirect(new URL(routes.dashboard.home(), req.url));
+      response.cookies.set('has-redirected', 'true', { path: '/', maxAge: 60 });
+      return response;
     }
   } else {
     // Redirect to home if not logged in and trying to access protected routes
